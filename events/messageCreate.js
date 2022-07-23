0331009642b2b4f3c6b1e5871e2d969a958e7159
chillyixa <chillyixa@gmail.com>
2022-07-23 09:16:31 +0300

    Prefix debug

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -3,8 +3,12 @@ const client = require("../index");
 const config = require("../config.json");
 const {GuildSettings} = require("../schema")
 client.on("messageCreate", async (message) => {
-  const prefix = await GuildSettings.findOne({guildId: message.guild.id}).prefix || config.prefix
+  const getGuildData = await GuildSettings.findOne({guildId: message.guild.id})
+  var defaultPrefix = config.prefix
+  var prefix = getGuildData ? getGuildData.prefix : defaultPrefix
   if (
+    !message.content.startsWith(prefix) &&
+    !message.content.startsWith(defaultPrefix) ||
     message.author.bot ||
     message.channel.type === "dm"
   )
